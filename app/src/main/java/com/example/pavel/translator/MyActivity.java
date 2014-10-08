7d13a6ca539731170d4d0a6a6d9ef2d1f2eac571
salting <salting@bk.ru>
2014-10-08 17:24:53 +0400

    fragments

--- app/src/main/java/com/example/pavel/translator/MyActivity.java
+++ app/src/main/java/com/example/pavel/translator/MyActivity.java
@@ -1,36 +1,53 @@
 package com.example.pavel.translator;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
+import java.util.HashMap;
 
 public class MyActivity extends Activity {
+
+    public Fragment PreloaderFragment;
+    public Fragment TranslatorFragment;
+    public FragmentTransaction TransFragment;
+    public BroadcastReceiver Broadcast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
+
+        Log.d("LOG_TAG", "My Activity 000");
+
+        TranslatorFragment = new TranslatorFragment();
+        TransFragment = getFragmentManager().beginTransaction();
+
+        TransFragment.add(R.id.mainActivity, TranslatorFragment);
+        TransFragment.commit();
+
+        Broadcast = new BroadcastReceiver() {
+
+            public void onReceive(Context context, Intent intent) {
+                Object langs = intent.getExtras().get("Langs");
+                Log.d("LOG_TAG", langs.toString());
+            }
+
+        };
+
+        Log.d("LOG_TAG", "My Activity 111");
+
+        startService(new Intent(this, TranslatorService.class));
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        Log.d("LOG_TAG", "My Activity 222");
+
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
